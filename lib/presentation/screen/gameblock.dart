40f9ceb0d90a82d3195f142eead19b111b2428bf
stokmakov <stokmakov@mts.ru>
2024-05-15 01:23:11 +0300

    second

---
+++ lib/presentation/screen/gameblock.dart
@@ -0,0 +1,294 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stack/stack.dart';
+
+abstract class CounterEvent {}
+
+class InitGame extends CounterEvent {}
+class PlayerHitCard extends CounterEvent {}
+class PlayerHold extends CounterEvent {}
+class DealerHitCard extends CounterEvent {}
+
+abstract class GameState {}
+class Initial extends GameState {}
+class Started extends GameState {
+  Deck deck;
+  Player player;
+  Player dealer;
+
+  Started({required this.deck, required this.player, required this.dealer});
+}
+class PlayerDraw extends Started {
+  PlayerDraw({required super.deck, required super.player, required super.dealer});
+}
+class DealerDraw extends Started {
+  DealerDraw({required super.deck, required super.player, required super.dealer});
+}
+class DealerWin extends Started {
+  DealerWin({required super.deck, required super.player, required super.dealer});
+}
+class PlayerWin extends Started {
+  PlayerWin({required super.deck, required super.player, required super.dealer});
+}
+
+class GameBloc extends Bloc<CounterEvent, GameState> {
+  GameBloc() : super(Initial()) {
+    on<InitGame>((event, emit) => emit(_initializeGame(event, state)));
+    on<PlayerHitCard>((event, emit) => emit(_playerHitCard(state)));
+    on<PlayerHold>((event, emit) => emit(_playerHold(state)));
+    on<DealerHitCard>((event, emit) => emit(_dealerHitCard(state)));
+  }
+
+  GameState _initializeGame(InitGame event, GameState currentState) {
+    switch (currentState) {
+      case DealerWin _:
+      case PlayerWin _:
+      case Initial _:
+        Deck deck = Deck();
+        Player dealer = Player(hand: [deck.hit(), deck.hit()]);
+        Player player = Player(hand: [deck.hit(), deck.hit()]);
+        // bool dealerBlackJack = _isBlackJack(dealer.hand);
+        // if (dealerBlackJack) {
+        //   print("DealerWin");
+        // }
+        bool playerBlackJack = _isBlackJack(player.hand);
+        if (playerBlackJack) {
+          // TODO: If dealer have ten point, then show dialog, else Player win
+          return PlayerWin(
+              deck: deck,
+              dealer: dealer,
+              player: player
+          );
+        }
+        return PlayerDraw(
+            deck: deck,
+            dealer: dealer,
+            player: player
+        );
+    }
+    return currentState;
+  }
+
+  GameState _playerHitCard(GameState currentState) {
+    switch (currentState) {
+      case PlayerDraw s1:
+        s1.player.hand.add(s1.deck.hit());
+        if (_isOverflow(s1.player.hand)) {
+          return DealerWin(
+              deck: s1.deck,
+              dealer: s1.dealer,
+              player: s1.player
+          );
+        } else if (_isBlackJack(s1.player.hand)) {
+          // TODO: stop player hit and start dealer draw
+          return PlayerWin(
+              deck: s1.deck,
+              dealer: s1.dealer,
+              player: s1.player
+          );
+        }
+        return PlayerDraw(
+          deck: s1.deck,
+          player: s1.player,
+          dealer: s1.dealer
+        );
+    }
+    return currentState;
+  }
+
+  GameState _playerHold(GameState currentState) {
+    switch (currentState) {
+      case PlayerDraw s1: {
+        if (_isOverflow(s1.dealer.hand)) {
+          return PlayerWin(
+              deck: s1.deck,
+              dealer: s1.dealer,
+              player: s1.player
+          );
+        } else if (_isBlackJack(s1.dealer.hand)) {
+          return DealerWin(
+              deck: s1.deck,
+              dealer: s1.dealer,
+              player: s1.player
+          );
+        }
+        return DealerDraw(
+            deck: s1.deck,
+            player: s1.player,
+            dealer: s1.dealer
+        );
+      }
+    }
+    return currentState;
+  }
+
+  GameState _dealerHitCard(GameState currentState) {
+    switch (currentState) {
+      case DealerDraw s1:
+        s1.dealer.hand.add(s1.deck.hit());
+        // TODO: apply dealer strategy
+        if (_isOverflow(s1.dealer.hand)) {
+          return PlayerWin(
+              deck: s1.deck,
+              dealer: s1.dealer,
+              player: s1.player
+          );
+        } else if (_isBlackJack(s1.dealer.hand)) {
+          return DealerWin(
+              deck: s1.deck,
+              dealer: s1.dealer,
+              player: s1.player
+          );
+        }
+        return DealerDraw(
+            deck: s1.deck,
+            player: s1.player,
+            dealer: s1.dealer
+        );
+    }
+    return currentState;
+  }
+
+  bool _isBlackJack(List<PlayingCard> cards) {
+    int value = _getValue(cards);
+    return value == 21;
+  }
+
+  bool _isOverflow(List<PlayingCard> cards) {
+    int value = _getValue(cards);
+    return value > 21;
+  }
+
+  int _getValue(List<PlayingCard> cards) {
+    int value = 0;
+    for (var element in cards) {
+      // Check if ACE
+      value += element.value;
+    }
+    return value;
+  }
+}
+
+class Player {
+  List<PlayingCard> hand;
+  Player({required this.hand});
+}
+
+class Deck {
+  Stack<PlayingCard> cards = Stack();
+  Deck() {
+    List<PlayingCard> shuffled = List<PlayingCard>.empty(growable: true);
+    shuffled.addAll(
+        _generateCards(CardSuit.CLUBS)
+    );
+    shuffled.addAll(
+        _generateCards(CardSuit.DIAMONDS)
+    );
+    shuffled.addAll(
+        _generateCards(CardSuit.HEARTS)
+    );
+    shuffled.addAll(
+        _generateCards(CardSuit.SPADES)
+    );
+
+    shuffled.shuffle();
+    for (var element in shuffled) {
+      cards.push(element);
+    }
+  }
+
+  // IllegalOperationException
+  PlayingCard hit() {
+    return cards.pop();
+  }
+
+  List<PlayingCard> _generateCards(CardSuit suit) {
+    List<PlayingCard> cards = List<PlayingCard>.generate(13, (index) {
+      PlayingCard card;
+      if (index < 9) {
+        card = NumeralsCard(
+            value: index+2,
+            suite: suit
+        );
+      } else if (index == 9) {
+        card = CourtCard(
+            type: Court.JACK,
+            value: 10,
+            suite: suit
+        );
+      } else if (index == 10) {
+        card = CourtCard(
+            type: Court.QUEEN,
+            value: 10,
+            suite: suit
+        );
+      } else if (index == 11) {
+        card = CourtCard(
+            type: Court.KING,
+            value: 10,
+            suite: suit
+        );
+      } else {
+        card = AceCard(
+            value: 11,
+            suite: suit
+        );
+      }
+      return card;
+    });
+    return cards;
+  }
+}
+
+class PlayingCard {
+  int value;
+  CardSuit suite;
+
+  PlayingCard({required this.value, required this.suite});
+}
+
+class NumeralsCard extends PlayingCard {
+  NumeralsCard({required super.value, required super.suite});
+}
+
+class CourtCard extends PlayingCard {
+  Court type;
+  CourtCard({required this.type, required super.value, required super.suite});
+}
+
+class AceCard extends PlayingCard {
+  int minVal = 1;
+  AceCard({required super.value, required super.suite});
+}
+
+enum Court {
+  JACK("jack"),
+  QUEEN("queen"),
+  KING("king");
+
+  const Court(this.name);
+  final String name;
+}
+
+enum CardSuit {
+  HEARTS("hearts"),
+  DIAMONDS("diamonds"),
+  CLUBS("clubs"),
+  SPADES("spades");
+
+  const CardSuit(this.name);
+  final String name;
+}
+
+extension CardIcon on PlayingCard {
+  String get asset {
+    const assetPath = "images/cards";
+    if (this is NumeralsCard) {
+      return "$assetPath/${suite.name}_$value.svg";
+    } else if (this is CourtCard) {
+      return "$assetPath/${suite.name}_${(this as CourtCard).type.name}.png";
+    } else if (this is AceCard) {
+      return "$assetPath/${suite.name}_ace.svg";
+    }
+    throw "Unsupported Card type $this";
+  }
+}
---
+++ lib/presentation/screen/mainscreen.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../theme/theme.dart';
+import 'gameblock.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => GameBloc(),
+      child: const CounterView(),
+    );
+  }
+}
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Black Jack')),
+      body: Center(
+        child: BlocBuilder<GameBloc, GameState>(
+          builder: (context, state) {
+            Widget widget;
+            switch (state) {
+              case Initial _: widget = _renderInitialState(
+                  theme, () { context.read<GameBloc>().add(InitGame()); }
+              );
+              case PlayerDraw s1: widget = _renderPlayerDraw(s1, theme);
+              case DealerDraw s1:
+                // TODO: send delayed action
+                Future.delayed(const Duration(seconds: 2),() => {
+                  context.read<GameBloc>().add(DealerHitCard())
+                });
+                widget = _renderDealerDraw(s1, theme);
+              case DealerWin s1: widget = _renderDealerWin(context, s1, theme);
+              case PlayerWin s1: widget = _renderPlayerWin(context, s1, theme);
+
+              default: widget = Text(
+                "$state",
+                style: theme.textTheme.displayLarge,
+              );
+            }
+            return widget;
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Text("Take", style: theme.textTheme.displayLarge,),
+            // child: const Icon(Icons.add),
+            onPressed: () {
+              context.read<GameBloc>().add(PlayerHitCard());
+            },
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: Text("Hold", style: theme.textTheme.displayLarge,),
+            // child: const Icon(Icons.remove),
+            onPressed: () {
+              context.read<GameBloc>().add(PlayerHold());
+            },
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.brightness_6),
+            onPressed: () {
+              context.read<ThemeCubit>().toggleTheme();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _renderInitialState(ThemeData theme, VoidCallback onPressed) {
+    return Container(
+      color: theme.scaffoldBackgroundColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+              onPressed: onPressed,
+              child: Text(
+                "Start",
+                style: theme.textTheme.displayLarge,
+              )
+          ),
+          SvgPicture.asset(
+            "images/cards/clubs_2.svg",
+            semanticsLabel: "My SVG Image",
+            height: 150,
+            width: 105,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _renderPlayerDraw(PlayerDraw state, ThemeData theme) {
+    return _renderGameStarted(state, theme);
+  }
+
+  Widget _renderDealerDraw(DealerDraw state, ThemeData theme) {
+    return _renderGameStarted(state, theme);
+  }
+
+  Widget _renderDealerWin(BuildContext context, DealerWin state, ThemeData theme) {
+    Future.microtask(() => showEndGameDialog(context, "Dealer WIN!"));
+    return _renderGameStarted(state, theme);
+  }
+
+  Widget _renderPlayerWin(BuildContext context, PlayerWin state, ThemeData theme) {
+    Future.microtask(() => showEndGameDialog(context, "You WIN!"));
+    return _renderGameStarted(state, theme);
+  }
+
+  Widget _renderGameStarted(Started state, ThemeData theme) {
+    List<Widget> dealerCards = [];
+    for (var element in state.dealer.hand) {
+      // && state is PlayerDraw
+      if (state.dealer.hand.first == element && state is PlayerDraw) {
+        dealerCards.add(
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Image.asset(
+                "images/cards/suit_red.png",
+                height: 150,
+                width: 105,
+              ),
+            )
+        );
+        continue;
+      }
+      dealerCards.add(
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: _getCardOf(element),
+          )
+      );
+    }
+    List<Widget> playerCards = [];
+    for (var element in state.player.hand) {
+      playerCards.add(
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: _getCardOf(element),
+          )
+      );
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "Dealer",
+          style: theme.textTheme.displayLarge,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: dealerCards,
+        ),
+        Text(
+          "Player",
+          style: theme.textTheme.displayLarge,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: playerCards,
+        ),
+      ],
+    );
+  }
+
+  Widget _getCardOf(PlayingCard element) {
+    Widget result;
+    if (element is CourtCard) {
+      result = Image.asset(
+        element.asset,
+        height: 150,
+        width: 105,
+      );
+    } else {
+      result = SvgPicture.asset(
+        element.asset,
+        height: 150,
+        width: 105,
+      );
+    }
+    return result;
+  }
+
+  void showEndGameDialog(BuildContext context, String text) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext inner) {
+        return AlertDialog(
+          title: Text(text),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Would you like to po4esat\' kolodu?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Play'),
+              onPressed: () {
+                context.read<GameBloc>().add(InitGame());
+                Navigator.of(inner).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
